7780e38f8e07f9fd2153c5fd75e53db1c8da6ad1
Toni Hukka <a1700324@myy.haaga-helia.fi>
2018-05-13 13:24:00 +0300

    Tweaking the script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 # Original code: Tero Karvinen https://github.com/terokarvinen/sirotin/
 # Run this script if you _don't have_ Salt Minion and/or Git installed.
+# If you have them installed, run suola.sh
 
 echo "Starting automatic Fail2Ban installation and configuration"
-set -o verbose
+# set -o verbose
 
 echo "Installing Salt Minion and Git"
 sudo apt-get update
@@ -12,6 +13,6 @@ echo "Setting up Salt and copying Git repository"
 echo 'master: localhost'|sudo tee /etc/salt/minion
 git clone https://github.com/banaanivohveli/fail2ban.git
 cd fail2ban/
-./suola.sh
+bash suola.sh
 
 echo "Done! Domo arigato, Mr. Roboto"
